2ebb43fd99728e28ad622aca3aee1e3413b22742
Chelimela Sai Vamshi <chsaivamshi054@gmail.com>
2024-08-13 22:41:53 +0530

    feat:(customer-service): add customer saving logic with duplicate check

--- src/com/vamshi/foodiecliapp/service/CustomerServiceImpl.java
+++ src/com/vamshi/foodiecliapp/service/CustomerServiceImpl.java
@@ -7,7 +7,7 @@ import com.vamshi.foodiecliapp.repository.CustomerRepository;
 import java.util.Optional;
 
 public class CustomerServiceImpl implements CustomerService{
-    private final CustomerRepository customerRepository;
+    private final  CustomerRepository customerRepository;
 
     public CustomerServiceImpl(CustomerRepository customerRepository) {
         this.customerRepository = customerRepository;
